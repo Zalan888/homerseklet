e0a28215c7481f935c1c7ee7b51ec4017ed58b5c
Zalan888 <witmannz@kkszki.hu>
2024-10-18 10:33:12 +0200

    Added temps array, and input function

--- Program.cs
+++ Program.cs
@@ -8,6 +8,17 @@ namespace homerseklet
 {
     internal class Program
     {
+        static int[] temps = new int[7];
+
+        static void input()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Kérem a(z) {i + 1}. nap hőmérsékletét");
+                temps[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
         static void Main(string[] args)
         {
         }
